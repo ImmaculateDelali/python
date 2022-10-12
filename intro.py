bad8b7a3fdd10e146560add2bf21124d228ad4a0
Immaculate Delali <florenceahorlu@meltwater.org>
2022-10-12 08:48:44 +0000

    second commit

---
+++ intro.py
@@ -0,0 +1,6 @@
+# first assignment in python
+firstNum= input("Input first number: ")
+secondNum= input("Input second number: ")
+sum = int(firstNum) + int(secondNum)
+print('The sum of {0} and {1} is {2}'.format(firstNum,secondNum,sum))
+
